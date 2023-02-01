7ac090a19762e969c04d0e88cc7a8b2b6e4b30f6
PreetAsari15 <preetasari@gmail.com>
2023-02-01 15:39:11 -0600

    install dependencies React+tailwindcss

--- admin/src/pages/DetailedOrder/index.js
+++ admin/src/pages/DetailedOrder/index.js
@@ -1,78 +1,43 @@
 import React from "react";
 import { Header } from "../../components";
-import { ListBoxComponent } from "@syncfusion/ej2-react-dropdowns";
-
+import { ListViewComponent } from "@syncfusion/ej2-react-lists";
+import { dishes } from "../../../src/resources/dishes.json";
+import Logo from "../../resources/avatar.jpg";
 const DetailedOrder = () => {
   return (
     <div>
       <Header category="Page" title={"Detailed Order"} />
-      <div className="flex justify-center" style={{ width: 500, height: 100 }}>
-        <ListBoxComponent
-          label="Customer"
-          dataSource={["LukasGraham"]}
-        ></ListBoxComponent>
-        <ListBoxComponent
-          label="Customer Address"
-          dataSource={["43 Aditya Bunglows, Regina, Saskatchewan"]}
-        ></ListBoxComponent>
+      <div className="flex justify-center" style={{ width: 500, height: 150 }}>
+        <div className="e-card">
+          <div className="e-card-header">
+            <div className="e-card-header-image football" />
+            <img
+              src={Logo}
+              className="rounded-full w-12 h-12"
+              alt="Didn't work init"
+            />
+            <div className="e-card-header-caption">
+              <div className="e-card-header-title"> Carson Callahan</div>
+              <div className="e-card-sub-title">
+                4045 Robinson St, Regina, SK
+              </div>
+            </div>
+          </div>
+          <div className="e-card-content">
+            Carson received a BA in psychology from the University of
+            Washington. She has also completed a course in business French. She
+            reads and writes French.
+          </div>
+        </div>
+      </div>
+      <div
+        style={{ width: 500, height: 150 }}
+        className="divide-y divide-dashed md:divide-solid mt-3"
+      >
+        <div></div>
+        <div></div>
       </div>
     </div>
-
-    // <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-    //   <Header category="Page" title="Detailed Order" />
-    //   <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-    //     <div className="px-4 py-5 sm:px-6">
-    //       <h3 className="text-lg font-medium leading-6 text-gray-900">
-    //         Customer
-    //       </h3>
-    //       <p className="mt-1 max-w-2xl text-sm text-gray-500">
-    //         Customer details and application.
-    //       </p>
-    //     </div>
-    //     <div className="border-t border-gray-200">
-    //       <dl>
-    //         <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-    //           <dt
-    //             className="text-sm font-medium text-gray-500"
-    //             label="Customer"
-    //           >
-    //             Full Name
-    //           </dt>
-    //           <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-    //             Carson Foster
-    //           </dd>
-    //         </div>
-    //         <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-    //           <dt className="text-sm font-medium text-gray-500">
-    //             Customer Address
-    //           </dt>
-    //           <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-    //             Retallack St, Saskatoon, SK, Canada
-    //           </dd>
-    //         </div>
-    //         <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-    //           <dt className="text-sm font-medium text-gray-500">
-    //             Salary expectation
-    //           </dt>
-    //           <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-    //             $120,000
-    //           </dd>
-    //         </div>
-    //         <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-    //           <dt className="text-sm font-medium text-gray-500">About</dt>
-    //           <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-    //             Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
-    //             incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
-    //             consequat sint. Sit id mollit nulla mollit nostrud in ea officia
-    //             proident. Irure nostrud pariatur mollit ad adipisicing
-    //             reprehenderit deserunt qui eu.
-    //           </dd>
-    //         </div>
-    //         <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"></div>
-    //       </dl>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
