be4026a9183f2d00b7fdb4b2d10548f6fbde756f
PreetAsari15 <preetasari@gmail.com>
2023-02-05 10:52:03 -0600

    install ej2-base syncfusion lib
    claim license key from syncfusion dashboard to avoid warning prompt 🙂

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
 import { ContextProvider } from "./contexts/ContextProvider";
+import { registerLicense } from "@syncfusion/ej2-base";
+
+// Registering Syncfusion license key
+registerLicense("register key");
 
 ReactDOM.render(
   <ContextProvider>
