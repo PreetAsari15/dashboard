c3a1a968c4e18f9b2f368a4629adfec3e42c976f
PreetAsari15 <preetasari@gmail.com>
2023-02-06 21:25:31 -0600

    dummy data added for testing image rendering with name of wasteprovider and their fees

--- client/App.js
+++ client/App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* WasteProvider item */}
+      <View style={styles.wasteproviderContainer}>
+        <Image
+          source={{
+            uri: "https://www.quantumbooks.com/wp-content/uploads/2018/08/Waste-management.jpeg",
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.title}> Think Clean Think Clean </Text>
+        <Text style={styles.subtitle}> $25.99 15-30 minutes </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  wasteproviderContainer: {
+    width: "100%",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 5 / 3,
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 5,
+  },
+  subtitle: {
+    color: "grey",
   },
 });
