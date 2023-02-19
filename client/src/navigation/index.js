cce75e41774fc3e7b58b8e86bf132690b4e620ee
PreetAsari15 <preetasari@gmail.com>
2023-02-18 22:55:52 -0600

    bottom nav bar

--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import WasteProviderDetailsPage from "../screens/WasteProviderDetailsScreen";
 import WasteDetailsScreen from "../screens/WasteDetailsScreen";
@@ -6,7 +7,8 @@ import Basket from "../screens/Basket";
 import OrderScreen from "../screens/OrdersScreen";
 import OrderDetails from "../screens/OrderDetails";
 import { NavigationContainer } from "@react-navigation/native";
-
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
@@ -23,4 +25,40 @@ const RootNavigator = () => {
   );
 };
 
-export default RootNavigator;
+const Tab = createMaterialBottomTabNavigator();
+
+const HomeTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Orders"
+        component={OrderScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="list-alt" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={OrderScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Feather name="user" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default HomeTabs;
