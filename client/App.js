634888ad2ae0fcaa66b05c935a96921f0758f6de
PreetAsari15 <preetasari@gmail.com>
2023-02-19 16:44:02 -0600

    App.js cleaned from redundant imports
    DEPENDENCY ALERT:
    "npx expo install @react-native-community/netinfo@9.3.5"
    Use above if having issues with expo

--- client/App.js
+++ client/App.js
@@ -1,15 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image, FlatList } from "react-native";
-import WasteProviderItem from "./src/components/WasteProviderItem";
-import restaurants from "./assets/data/restaurants.json";
-import HomeScreen from "./src/screens/HomeScreen";
-import WasteProviderDetailsPage from "./src/screens/WasteProviderDetailsScreen";
-import WasteDetailsScreen from "./src/screens/WasteDetailsScreen";
-import Basket from "./src/screens/Basket";
-import OrderScreen from "./src/screens/OrdersScreen";
-import OrderDetails from "./src/screens/OrderDetails";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./src/navigation";
+import { Amplify } from "aws-amplify";
+import config from "./src/aws-exports";
 
 export default function App() {
   return (
