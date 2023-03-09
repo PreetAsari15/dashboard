ea2ee403cae55da776916c89663e6842b4b7279e
PreetAsari15 <preetasari@gmail.com>
2023-03-09 15:47:57 -0600

    order styling updates

--- driver/App.js
+++ driver/App.js
@@ -1,23 +1,57 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import orders from "./assets/data/orders.json";
+import { Entypo } from "@expo/vector-icons";
 
 const order = orders[0];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
+      <View
+        style={{
+          flexDirection: "row",
+          borderColor: "#3FC060",
+          borderWidth: 2,
+          borderRadius: 10,
+          margin: 10,
+        }}
+      >
         <Image
           source={{ uri: order.WasteProvider.image }}
-          style={{ width: 50, height: 50 }}
+          style={{
+            width: "25%",
+            height: "100%",
+            borderBottomLeftRadius: 10,
+            borderTopLeftRadius: 10,
+          }}
         />
-        <Text>{order.WasteProvider.name}</Text>
-        <Text>{order.WasteProvider.address}</Text>
-        <Text>Delivery Details</Text>
+        <View style={{ marginLeft: 10 }}>
+          <Text style={{ fontSize: 18, fontWeight: "500" }}>
+            {order.WasteProvider.name}
+          </Text>
+          <Text style={{ color: "grey" }}>{order.WasteProvider.address}</Text>
+          <Text style={{ marginTop: 10 }}>Delivery Details</Text>
 
-        <Text>{order.User.name}</Text>
-        <Text>{order.User.address}</Text>
+          <Text style={{ color: "grey" }}>{order.User.name}</Text>
+          <Text style={{ color: "grey" }}>{order.User.address}</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#3FC060",
+            borderBottomRightRadius: 10,
+            borderTopRightRadius: 10,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Entypo
+            name="check"
+            size={30}
+            color="white"
+            style={{ marginLeft: "auto" }}
+          />
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
