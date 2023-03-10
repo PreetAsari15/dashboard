ffd9f054b2dd968aaf3b799c25e89f48cd01cd3d
PreetAsari15 <preetasari@gmail.com>
2023-03-09 21:45:05 -0600

    OrderItem Page added
    Main page for displaying orders

--- driver/App.js
+++ driver/App.js
@@ -1,58 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, FlatList, View } from "react-native";
 import orders from "./assets/data/orders.json";
-import { Entypo } from "@expo/vector-icons";
-
-const order = orders[0];
+import OrderItem from "./src/component/OrderItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flexDirection: "row",
-          borderColor: "#3FC060",
-          borderWidth: 2,
-          borderRadius: 10,
-          margin: 10,
-        }}
-      >
-        <Image
-          source={{ uri: order.WasteProvider.image }}
-          style={{
-            width: "25%",
-            height: "100%",
-            borderBottomLeftRadius: 10,
-            borderTopLeftRadius: 10,
-          }}
-        />
-        <View style={{ marginLeft: 10 }}>
-          <Text style={{ fontSize: 18, fontWeight: "500" }}>
-            {order.WasteProvider.name}
-          </Text>
-          <Text style={{ color: "grey" }}>{order.WasteProvider.address}</Text>
-          <Text style={{ marginTop: 10 }}>Delivery Details</Text>
-
-          <Text style={{ color: "grey" }}>{order.User.name}</Text>
-          <Text style={{ color: "grey" }}>{order.User.address}</Text>
-        </View>
-        <View
-          style={{
-            backgroundColor: "#3FC060",
-            borderBottomRightRadius: 10,
-            borderTopRightRadius: 10,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Entypo
-            name="check"
-            size={30}
-            color="white"
-            style={{ marginLeft: "auto" }}
-          />
-        </View>
-      </View>
+      <FlatList
+        data={orders}
+        renderItem={({ item }) => <OrderItem order={item} />}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -62,7 +19,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    paddingTop: 50,
   },
 });
