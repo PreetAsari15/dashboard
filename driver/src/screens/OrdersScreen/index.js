630e938f9786b9c0d5ab9dbcbad9918e74ab6c58
PreetAsari15 <preetasari@gmail.com>
2023-03-10 20:15:53 -0600

    Testing updates
    Integration & System Testing done

--- driver/src/screens/OrdersScreen/index.js
+++ driver/src/screens/OrdersScreen/index.js
@@ -1,10 +1,17 @@
 import { useRef } from "react";
 import { View, Text } from "react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
 
 const OrdersScreen = () => {
+  const bottomSheetRef = useRef(null);
+
   return (
-    <View>
-      <Text>Hello World!</Text>
+    <View style={{ backgroundColor: "lightblue", flex: 1 }}>
+      <BottomSheet ref={bottomSheetRef} snapPoints={["12%", "95%"]}>
+        <View style={{ flex: 1 }}>
+          <Text>Hello</Text>
+        </View>
+      </BottomSheet>
     </View>
   );
 };
