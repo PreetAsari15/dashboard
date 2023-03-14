bd4774e827dc8a3f200304b7773f2d1bdaa68219
PreetAsari15 <preetasari@gmail.com>
2023-03-14 08:57:08 -0600

    implement live tracking
    Using MapView and useWindowDimensions hooks to render live location

--- driver/src/screens/OrdersScreen/index.js
+++ driver/src/screens/OrdersScreen/index.js
@@ -1,20 +1,29 @@
 import { useRef, useMemo } from "react";
-import { View, Text, FlatList, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Dimensions,
+  useWindowDimensions,
+} from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import orders from "../../../assets/data/orders.json";
 import OrderItem from "../../component/OrderItem";
 import MapView from "react-native-maps";
 const OrdersScreen = () => {
   const bottomSheetRef = useRef(null);
+  const { width, height } = useWindowDimensions();
 
   const snapPoints = useMemo(() => ["12%", "95%"], []);
   return (
     <View style={{ backgroundColor: "lightblue", flex: 1 }}>
       <MapView
         style={{
-          height: Dimensions.get("window").height,
-          width: Dimensions.get("window").width,
+          height,
+          width,
         }}
+        showsUserLocation
+        followsUserLocation
       />
       <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints}>
         <View style={{ alignItems: "center", marginBottom: 30 }}>
