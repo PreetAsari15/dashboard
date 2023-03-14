c0e080158312d9c617380075a909bf88854dbe5f
PreetAsari15 <preetasari@gmail.com>
2023-03-13 23:34:52 -0600

    Native MapView installed and rendered

--- driver/src/screens/OrdersScreen/index.js
+++ driver/src/screens/OrdersScreen/index.js
@@ -1,15 +1,21 @@
 import { useRef, useMemo } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Dimensions } from "react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import orders from "../../../assets/data/orders.json";
 import OrderItem from "../../component/OrderItem";
-
+import MapView from "react-native-maps";
 const OrdersScreen = () => {
   const bottomSheetRef = useRef(null);
 
   const snapPoints = useMemo(() => ["12%", "95%"], []);
   return (
     <View style={{ backgroundColor: "lightblue", flex: 1 }}>
+      <MapView
+        style={{
+          height: Dimensions.get("window").height,
+          width: Dimensions.get("window").width,
+        }}
+      />
       <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints}>
         <View style={{ alignItems: "center", marginBottom: 30 }}>
           <Text
